bd7cb303192b721809a1da3da8270c7359eca481
krislowg <46728217+krislowg@users.noreply.github.com>
2019-04-16 22:48:47 -0400

    pouvez

--- LowGIntegration/src/Nice.java
+++ LowGIntegration/src/Nice.java
@@ -3,6 +3,6 @@ public class Nice extends City {
 	//private String plageName;
 	
 	public void doActivities(){
-		System.out.println("In Nice, vous peutez aller a la plage Beau Rivage " /*+ plageName*/);
+		System.out.println("In Nice, vous pouvez aller a la plage Beau Rivage " /*+ plageName*/);
 	}
 }
