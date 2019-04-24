e655e838d2948fbc7c349e984e1959271db47e97
krislowg <46728217+krislowg@users.noreply.github.com>
2019-04-24 09:49:08 -0400

    Indentation

--- LowGIntegration/src/Nice.java
+++ LowGIntegration/src/Nice.java
@@ -1,8 +1,9 @@
 
 public class Nice extends City {
-	//private String plageName;
-	
-	public void doActivities(){
-		System.out.println("In Nice, vous pouvez aller a la plage Beau Rivage " /*+ plageName*/);
-	}
+  // private String plageName;
+
+  public void doActivities() {
+    System.out.println(
+        "In Nice, vous pouvez aller a la plage Beau Rivage " /* + plageName */);
+  }
 }
