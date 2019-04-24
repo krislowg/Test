825d5ead9f101d76e94d05a42dbcfdbcceaa540c
krislowg <46728217+krislowg@users.noreply.github.com>
2019-04-24 09:48:19 -0400

    Indentation

--- LowGIntegration/src/Chamonix.java
+++ LowGIntegration/src/Chamonix.java
@@ -1,18 +1,16 @@
 
 public class Chamonix extends City {
-	/*private String mountainName;
-		
-	public void Chamonix(String Language) {
-		super("French");
-	}
-	
-	
-	public String getMoN() {
-		return mountainName;
-	}
-	*/
-	public void doActivities(){
-		super.doActivities();
-		System.out.println("En Chamonix, vous pouvez faire du ski in la Aiguille du midi " /*+ mountainName*/);
-	}
+  /*
+   * private String mountainName;
+   * 
+   * public void Chamonix(String Language) { super("French"); }
+   * 
+   * 
+   * public String getMoN() { return mountainName; }
+   */
+  public void doActivities() {
+    super.doActivities();
+    System.out.println(
+        "En Chamonix, vous pouvez faire du ski en la Aiguille du midi " /* + mountainName */);
+  }
 }
