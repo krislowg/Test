22ef6b0eb8c80cf5443e385291a77345796100af
krislowg <46728217+krislowg@users.noreply.github.com>
2019-04-24 22:58:15 -0400

    Comments

--- LowGIntegration/src/Chamonix.java
+++ LowGIntegration/src/Chamonix.java
@@ -1,4 +1,10 @@
 
+/**
+ * Subclass. Child of City.
+ * Represents the things that you can do in Chamonix.
+ * @author Kristy Low
+ *
+ */
 public class Chamonix extends City {
   /*
    * private String mountainName;
@@ -7,6 +13,10 @@ public class Chamonix extends City {
    * 
    * 
    * public String getMoN() { return mountainName; }
+   */
+  
+  /* (non-Javadoc)
+   * @see City#doActivities()
    */
   public void doActivities() {
     super.doActivities();
